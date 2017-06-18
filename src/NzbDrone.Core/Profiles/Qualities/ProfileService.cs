acccb8bd6e456813f7180dcf8143399e50433de6
Mark McDowall <mark@mcdowall.ca>
2017-06-18 10:02:20 -0700

    Quality groups

--- src/NzbDrone.Core/Profiles/Qualities/ProfileService.cs
+++ src/NzbDrone.Core/Profiles/Qualities/ProfileService.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using NLog;
 using NzbDrone.Core.Lifecycle;
@@ -16,6 +16,7 @@ namespace NzbDrone.Core.Profiles.Qualities
         List<Profile> All();
         Profile Get(int id);
         bool Exists(int id);
+        Profile GetDefaultProfile(string name, Quality cutoff = null, params Quality[] allowed);
     }
 
     public class ProfileService : IProfileService, IHandle<ApplicationStartedEvent>
@@ -65,22 +66,7 @@ namespace NzbDrone.Core.Profiles.Qualities
         {
             return _profileRepository.Exists(id);
         }
-
-        private Profile AddDefaultProfile(string name, Quality cutoff, params Quality[] allowed)
-        {
-            var items = Quality.DefaultQualityDefinitions
-                            .OrderBy(v => v.Weight)
-                            .Select(v => new ProfileQualityItem { Quality = v.Quality, Allowed = allowed.Contains(v.Quality) })
-                            .ToList();
-
-            var profile = new Profile { Name = name, 
-                Cutoff = cutoff, 
-                Items = items, 
-            };
-
-            return Add(profile);
-        }
-
+        
         public void Handle(ApplicationStartedEvent message)
         {
             if (All().Any()) return;
@@ -89,42 +75,101 @@ namespace NzbDrone.Core.Profiles.Qualities
 
             AddDefaultProfile("Any", Quality.SDTV,
                 Quality.SDTV,
+                Quality.WEBRip480p,
                 Quality.WEBDL480p,
                 Quality.DVD,
                 Quality.HDTV720p,
                 Quality.HDTV1080p,
+                Quality.WEBRip720p,
                 Quality.WEBDL720p,
+                Quality.WEBRip1080p,
                 Quality.WEBDL1080p,
                 Quality.Bluray720p,
                 Quality.Bluray1080p);
 
             AddDefaultProfile("SD", Quality.SDTV,
                 Quality.SDTV,
+                Quality.WEBRip480p,
                 Quality.WEBDL480p,
                 Quality.DVD);
 
             AddDefaultProfile("HD-720p", Quality.HDTV720p,
                 Quality.HDTV720p,
+                Quality.WEBRip720p,
                 Quality.WEBDL720p,
                 Quality.Bluray720p);
 
             AddDefaultProfile("HD-1080p", Quality.HDTV1080p,
                 Quality.HDTV1080p,
+                Quality.WEBRip1080p,
                 Quality.WEBDL1080p,
                 Quality.Bluray1080p);
 
             AddDefaultProfile("Ultra-HD", Quality.HDTV2160p,
                 Quality.HDTV2160p,
+                Quality.WEBRip2160p,
                 Quality.WEBDL2160p,
                 Quality.Bluray2160p);
 
             AddDefaultProfile("HD - 720p/1080p", Quality.HDTV720p,
                 Quality.HDTV720p,
                 Quality.HDTV1080p,
+                Quality.WEBRip720p,
                 Quality.WEBDL720p,
+                Quality.WEBRip1080p,
                 Quality.WEBDL1080p,
                 Quality.Bluray720p,
                 Quality.Bluray1080p);
+        }
+
+        public Profile GetDefaultProfile(string name, Quality cutoff = null, params Quality[] allowed)
+        {
+            var groupedQualites = Quality.DefaultQualityDefinitions.GroupBy(q => q.Weight);
+            var items = new List<ProfileQualityItem>();
+            var groupId = 1000;
+
+            foreach (var group in groupedQualites)
+            {
+                if (group.Count() == 1)
+                {
+                    var quality = group.First().Quality;
+
+                    items.Add(new ProfileQualityItem { Quality = group.First().Quality, Allowed = allowed.Contains(quality) });
+                    continue;
+                }
+
+                var groupAllowed = group.Any(g => allowed.Contains(g.Quality));
+
+                items.Add(new ProfileQualityItem
+                {
+                    Id = groupId,
+                    Name = group.First().GroupName,
+                    Items = group.Select(g => new ProfileQualityItem
+                    {
+                        Quality = g.Quality,
+                        Allowed = groupAllowed
+                    }).ToList(),
+                    Allowed = groupAllowed
+                });
+
+                groupId++;
+            }
+
+            var qualityProfile = new Profile
+                                 {
+                                     Name = name,
+                                     Cutoff = cutoff == null ? Quality.Unknown.Id : cutoff.Id,
+                                     Items = items
+                                 };
+
+            return qualityProfile;
+        }
+
+        private Profile AddDefaultProfile(string name, Quality cutoff, params Quality[] allowed)
+        {
+            var profile = GetDefaultProfile(name, cutoff, allowed);
+
+            return Add(profile);
         }
     }
 }

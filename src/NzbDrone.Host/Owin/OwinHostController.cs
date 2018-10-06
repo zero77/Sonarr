cdfeccc31fd864e1962aadcbe7a23b4b2fc2854e
Mark McDowall <mark@mcdowall.ca>
2018-10-06 08:41:36 -0700

    fixup! v3 API

--- src/NzbDrone.Host/Owin/OwinHostController.cs
+++ src/NzbDrone.Host/Owin/OwinHostController.cs
@@ -7,26 +7,26 @@ namespace NzbDrone.Host.Owin
     public class OwinHostController : IHostController
     {
         private readonly IOwinAppFactory _owinAppFactory;
-        private readonly IRemoteAccessAdapter _removeAccessAdapter;
+        private readonly IRemoteAccessAdapter _remoteAccessAdapter;
         private readonly IUrlAclAdapter _urlAclAdapter;
         private readonly Logger _logger;
         private IDisposable _owinApp;
 
         public OwinHostController(
                                   IOwinAppFactory owinAppFactory,
-                                  IRemoteAccessAdapter removeAccessAdapter,
+                                  IRemoteAccessAdapter remoteAccessAdapter,
                                   IUrlAclAdapter urlAclAdapter,
                                   Logger logger)
         {
             _owinAppFactory = owinAppFactory;
-            _removeAccessAdapter = removeAccessAdapter;
+            _remoteAccessAdapter = remoteAccessAdapter;
             _urlAclAdapter = urlAclAdapter;
             _logger = logger;
         }
 
         public void StartServer()
         {
-            _removeAccessAdapter.MakeAccessible(true);
+            _remoteAccessAdapter.MakeAccessible(true);
 
             _logger.Info("Listening on the following URLs:");
             foreach (var url in _urlAclAdapter.Urls)
